51504d00cce1d862218121b5160ec9bcb441bfa1
rhonvyramos <rhonvyramos@gmail.com>
2023-10-18 13:28:17 -0400

    imported useMutation into SearchBooks

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -8,6 +8,9 @@ import {
   Row
 } from 'react-bootstrap';
 
+// importing useMutation
+import { useMutation } from "@apollo/client";
+
 import Auth from '../utils/auth';
 import { saveBook, searchGoogleBooks } from '../utils/API';
 import { saveBookIds, getSavedBookIds } from '../utils/localStorage';
