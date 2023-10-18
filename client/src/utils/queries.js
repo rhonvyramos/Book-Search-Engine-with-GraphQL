e498b11db7dab3b930cb760b301e6a1616889fad
rhonvyramos <rhonvyramos@gmail.com>
2023-10-18 11:51:31 -0400

    GET_ME gql query fully implemented; gql queries all user's data, including saved books

--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -1,4 +1,20 @@
 import { gql } from "@apollo/client";
 
 export const GET_ME = gql`
+{
+    me {
+        _id
+        username
+        email
+        bookCount
+        savedBooks {
+                bookId
+                authors
+                title
+                decription
+                image
+                link
+        }
+    }
+}
 `;
