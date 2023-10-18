2ea16517120afa849733973b81d59b9a5ad20879
rhonvyramos <rhonvyramos@gmail.com>
2023-10-18 13:33:26 -0400

    fixed mispelling of variable initialization in SearchBooks.js; renamed conts to const

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -27,6 +27,9 @@ const SearchBooks = () => {
   // create state to hold saved bookId values
   const [savedBookIds, setSavedBookIds] = useState(getSavedBookIds());
 
+  // initializing the logic for useMutation with SAVE_BOOK as parameter
+  const [saveBook, { error }] = useMutation(SAVE_BOOK);
+
   // set up useEffect hook to save `savedBookIds` list to localStorage on component unmount
   // learn more here: https://reactjs.org/docs/hooks-effect.html#effects-with-cleanup
   useEffect(() => {
