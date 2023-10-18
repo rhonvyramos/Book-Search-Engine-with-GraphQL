7f2562618f3260ebb5e199d2e6daa69db9c65729
rhonvyramos <rhonvyramos@gmail.com>
2023-10-18 13:56:29 -0400

    added comment explaining purpose of login_user

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from "@apollo/client";
 
+// mutation used for user login functionality
 export const LOGIN_USER = gql`
     mutation login($email: String!, $password: String!) {
         login(email: $email, password: $password) {
